1d88d94741651964599673082cf4019b797021d2
rajesh <rajeshupadhayaya@salud.unm.edu>
2025-07-18 16:53:48 -0600

    Make the pushking to take parameters

---
+++ graph_creation/example/run_pushkin.sh
@@ -0,0 +1,10 @@
+python graph_creation/pushkin.py --config depression_alzheimers \
+  --output-dir graph_creation/result \
+  --first-degree-threshold 100 \
+  --host localhost \
+  --port 5432 \
+  --dbname causalehr \
+  --user rajesh \
+  --password Usps@6855 \
+  --schema causalehr \
+  --verbose
