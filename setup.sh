19fa7cc12aa6b0a38f093ff2ef8c807a315db2cd
Rajesh <rajeshupadhayaya68@gmail.com>
2025-09-19 10:51:15 -0600

    Updated the documents

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create and activate conda environment
+conda env create -f doc/environment.yml
+conda activate causalknowledgetrace
+
+# Install R packages from packages.R
+Rscript doc/packages.R
