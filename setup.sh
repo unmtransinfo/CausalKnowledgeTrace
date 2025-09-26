b7fe41727374f442a6139899f9c8144c739a76e1
rajesh <rajeshupadhayaya@salud.unm.edu>
2025-09-26 16:32:21 -0600

    Updated the readme and documnet

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ conda activate causalknowledgetrace
 
 # Install R packages from packages.R
 Rscript doc/packages.R
+
+# Create CUI search index table
+psql -h "$DB_HOST" -p "${DB_PORT:-5432}" -U "$DB_USER" -d "$DB_NAME" -f doc/create_cui_search_table.sql
